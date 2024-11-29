6fbd069a0954982ea399ee401130bd7e53ec8231
macydnah <86342863+macydnah@users.noreply.github.com>
2024-11-29 01:48:17 -0600

    refactor: check in `pdf.lua` plugin for template

--- init.lua
+++ init.lua
@@ -1,10 +1,47 @@
---- @sync entry
+local M = {}
 
-local function setup(self, opts) self.open_multi = opts.open_multi end
+function M:peek()
+	local start, cache = os.clock(), ya.file_cache(self)
+	if not cache or self:preload() ~= 1 then
+		return
+	end
 
-local function entry(self)
-	local h = cx.active.current.hovered
-	ya.manager_emit(h and h.cha.is_dir and "enter" or "open", { hovered = not self.open_multi })
+	ya.sleep(math.max(0, PREVIEW.image_delay / 1000 + start - os.clock()))
+	ya.image_show(cache, self.area)
+	ya.preview_widgets(self, {})
 end
 
-return { entry = entry, setup = setup }
+function M:seek(units)
+	local h = cx.active.current.hovered
+	if h and h.url == self.file.url then
+		local step = ya.clamp(-1, units, 1)
+		ya.manager_emit("peek", { math.max(0, cx.active.preview.skip + step), only_if = self.file.url })
+	end
+end
+
+function M:preload()
+	local cache = ya.file_cache(self)
+	if not cache or fs.cha(cache) then
+		return 1
+	end
+
+	local output = Command("pdftoppm")
+		:args({ "-singlefile", "-jpeg", "-jpegopt", "quality=75", "-f", tostring(self.skip + 1), tostring(self.file.url) })
+		:stdout(Command.PIPED)
+		:stderr(Command.PIPED)
+		:output()
+
+	if not output then
+		return 0
+	elseif not output.status.success then
+		local pages = tonumber(output.stderr:match("the last page %((%d+)%)")) or 0
+		if self.skip > 0 and pages > 0 then
+			ya.manager_emit("peek", { math.max(0, pages - 1), only_if = self.file.url, upper_bound = true })
+		end
+		return 0
+	end
+
+	return fs.write(cache, output.stdout) and 1 or 2
+end
+
+return M
