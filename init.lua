4e62bdcb8a1656074e74407db2bb44bb80b6cb40
macydnah <86342863+macydnah@users.noreply.github.com>
2025-02-11 22:35:48 -0600

    feat: update to `v25.2.71`
    As stated in the release notes in https://github.com/sxyazi/yazi/releases/tag/v25.2.7
    
    - `ya.md5()` has been deprecated in favor of the new `ya.hash()`
      sxyazi/yazi#2168
    
    - The plugin entry file `init.lua` has been deprecated in favor
      of the new `main.lua`
      sxyazi/yazi#2168
    
    - The numeric return value of the preloader has been deprecated in
      favor of a new boolean return value
      sxyazi/yazi#2253

--- init.lua
+++ init.lua
@@ -1,66 +1,114 @@
+--- @since 25.2.7
+
 local M = {}
 
-function M:peek()
-	local start, cache = os.clock(), ya.file_cache(self)
-	if not cache or self:preload() ~= 1 then
+function M:peek(job)
+	local start, cache = os.clock(), ya.file_cache(job)
+	if not cache then
+		return
+	end
+
+	local ok, err = self:preload(job)
+	if not ok or err then
 		return
 	end
 
 	ya.sleep(math.max(0, PREVIEW.image_delay / 1000 + start - os.clock()))
-	ya.image_show(cache, self.area)
+	ya.image_show(cache, job.area)
+	ya.preview_widgets(job, {})
 end
 
-function M:seek() end
+function M:seek(job)
+	local h = cx.active.current.hovered
+	ya.dbg("Linea 23")
+	ya.dbg("El url de cx.active.current.hovered es: " .. tostring(h.url))
+	if h and h.url == job.file.url then
+		local step = ya.clamp(-1, job.units, 1)
+		ya.dbg("Linea 27")
+		ya.dbg("El valor de los `steps` es: " .. tostring(step))
+		ya.dbg("Linea 29")
+		ya.dbg("El valor de cx.active.preview.skip es: " .. tostring(cx.active.preview.skip))
+		ya.dbg("Linea 31")
+		ya.dbg("El valor de `math.max(cx.active.preview.skip + step)` es: " .. math.max(0, cx.active.preview.skip + step))
+		ya.manager_emit("peek", { math.max(0, cx.active.preview.skip + step), only_if = job.file.url })
+	end
+end
 
-function M:document_to_pdf()
-	local tmp_dir = "/tmp/"
-	local pdf_file = tmp_dir .. self.file.name:gsub("%..*$", ".pdf")
+function M:document_to_pdf(job)
+--[[
+	if ya.target_family() = "unix" then
+		local user_id = ya.uid()
+	end
+--]]
+	local tmp = "/tmp/yazi-" .. ya.uid() .. "/office.yazi/"
+	ya.dbg("Linea 44: el valor de `tmp` es: " .. tmp)
+	local pdf_file = tmp .. job.file.name:gsub("%..*$", ".pdf")
+	ya.dbg("Linea 46")
+	ya.dbg("El valor de `job.skip + 1` es: " .. job.skip + 1)
 
-	read_perm = io.open(pdf_file, "r")
-	if read_perm then
-		read_perm:close()
-	else
-		local convert = Command("libreoffice"):args({
-				"--headless",
-				"--convert-to",
-				"pdf:draw_pdf_Export:{\"PageRange\":{\"type\":\"string\",\"value\":\"1\"}}",
-				"--outdir",
-				tmp_dir,
-				tostring(self.file.url)
-			})
-			:stdout(Command.NULL)
-			:stderr(Command.NULL)
-			:output()
+	local read_permissions = io.open(pdf_file, "r")
+	if read_permissions then
+		ya.dbg("Linea 51: si hay permisos de lectura")
+		read_permissions:close()
 	end
 
+	ya.dbg("Linea 55")
+	ya.dbg(tostring(job.file.url))
+	local convert = Command("libreoffice")
+		:args({
+			"--headless",
+			"--convert-to",
+			"pdf:draw_pdf_Export:{" ..
+				"\"PageRange\":{" ..
+					"\"type\":\"string\"," ..
+					"\"value\":" .. "\"" .. job.skip + 1 .. "\"" ..
+				"}" ..
+			"}",
+			"--outdir",
+			tmp,
+			tostring(job.file.url)
+		})
+		:stdout(Command.NULL)
+		:stderr(Command.NULL)
+		:output()
+	
 	return pdf_file
 end
 
-function M:preload()
-	local cache = ya.file_cache(self)
+function M:preload(job)
+	local cache = ya.file_cache(job)
+	ya.dbg("Linea 80")
+	ya.dbg(tostring(cache))
 	if not cache or fs.cha(cache) then
-		local cha, err = fs.cha(cache)
-		return 1
+		return true
 	end
 
-	local pdf_file = self:document_to_pdf()
-	local output = Command("pdftoppm")
-		:args({ "-singlefile", "-jpeg", "-jpegopt", "quality=75", "-f", tostring(self.skip + 1), tostring(pdf_file) })
+	local pdf_file = self:document_to_pdf(job)
+	local output, err = Command("pdftoppm")
+		:args({
+			"-singlefile",
+			"-jpeg",
+			"-jpegopt",
+			"quality=" .. PREVIEW.image_quality,
+			"-f",
+			1,
+			tostring(pdf_file),
+		})
 		:stdout(Command.PIPED)
 		:stderr(Command.PIPED)
 		:output()
 
 	if not output then
-		return 0
+		return true, Err("Failed to start `pdftoppm`, error: %s", err)
 	elseif not output.status.success then
 		local pages = tonumber(output.stderr:match("the last page %((%d+)%)")) or 0
-		if self.skip > 0 and pages > 0 then
-			ya.manager_emit("peek", { math.max(0, pages - 1), only_if = self.file.url, upper_bound = true })
+		if job.skip > 0 and pages > 0 then
+			ya.manager_emit("peek", { math.max(0, pages - 1), only_if = job.file.url, upper_bound = true })
 		end
-		return 0
+		return true, Err("Failed to convert PDF to image, stderr: %s", output.stderr)
 	end
 
-	return fs.write(cache, output.stdout) and 1 or 2
+	return fs.write(cache, output.stdout)
 end
 
 return M
